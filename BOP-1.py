2fd6ec30715bbae0b42732d8551a2debb37b7dd0
brauliolucia <5531276172brc@gmail.com>
2025-03-25 17:31:50 -0600

    variables

--- BOP-1.py
+++ BOP-1.py
@@ -1,8 +1,24 @@
+# Basics of Python
 # sintax 
 print ("Hello World")
-# comments
 
+# comments
 def sum(a,b):
-    return a+b
+    return a+b # the comments are indicated by the symbol #
 print(sum(1,2))
-# variables
+
+# variables --> a variable is a space in memory that stores a value
+# the value of a variable can be changed during the execution of the program
+variable3 = "3"
+variable1 = "1"
+variable2 = NotImplemented
+print (variable2, variable1 + variable3)
+
+# global variables
+global_variable = "I am a global variable"
+def function():
+    print (global_variable)
+
+    # modifying a global variable
+global_variable = "The global variable has been modified"
+function()
