3d74c8e325d125bc4dcd6e61154a7836b78c8e62
brauliolucia <5531276172brc@gmail.com>
2025-03-26 01:15:47 -0600

    aun no lo completo BOP-2

---
+++ BOP-2.py
@@ -0,0 +1,29 @@
+# Text Type: 	str
+# Numeric Types: 	int, float, complex
+# Sequence Types: 	list, tuple, range
+# Mapping Type: 	dict
+# Set Types: 	set, frozenset
+# Boolean Type: 	bool
+# Binary Types: 	bytes, bytearray, memoryview
+# None Type: 	NoneType
+
+element = 'text'
+
+list1 = ['list', 1, 2+3j, True]
+tuple1 = (1.2, 'tuple', 2+3j, None)
+list2 = ['segunda', 'lista', 'de', 'texto']
+
+elements = [list1, tuple1, list2
+            ]
+
+# for elementsIndex in elements:
+#         print(type(elementsIndex))
+
+for elementsIndex in elements:
+    while True:
+        if type(elementsIndex) is list:
+            print ('Is a list')
+            break
+        else:
+            print ('Not a list')
+            break
