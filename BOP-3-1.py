83551091e09b8e3430468ca2d970381a82b18d0d
brauliolucia <5531276172brc@gmail.com>
2025-03-30 20:38:43 -0600

    nuevo archivo de prueba

---
+++ BOP-3-1.py
@@ -0,0 +1,11 @@
+def modificar_con_memoryview():
+    datos = bytearray(b"Pythonista")  # Creamos un bytearray
+    print("Antes:", datos)
+
+    vista = memoryview(datos)  # Creamos la vista de memoria
+    vista[6:9] = b"XYZ"  # Modificamos una parte del bytearray sin copiarlo
+    
+    print("Después:", datos)
+
+# Ejecutar la función
+modificar_con_memoryview()
