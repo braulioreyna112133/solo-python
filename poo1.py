f55a223fd4260c86f44cd12a30152b4d24d2d487
brauliolucia <5531276172brc@gmail.com>
2025-04-01 20:57:07 -0600

    class, atributtes

--- poo1.py
+++ poo1.py
@@ -5,7 +5,7 @@ class pilots ():
         self.avgTimeResponse = avgTimeResponse
 
 # creando una instancia para llamada a la clase
-user = pilots (pathology='full',age=20,avgTimeResponse='fast')
+user = pilots (pathology='full', age=20, avgTimeResponse='fast')
 print (user.pathology, user.age, user.avgTimeResponse)
         
 
---
+++ poo2.py
@@ -0,0 +1,31 @@
+class Employee:
+    empCount = 0
+
+    # usamos __init__ para incializar la clase
+    def __init__ (self, name, salary): # parámetro self para hacer referencia a sí mismos
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount (self):
+        print ("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee (self):
+        print ("Name : ", self.name,  ", Salary: ", self.salary)
+
+# creando instancias -> llamando al objeto
+emp1 = Employee ("Zara", 2000)
+emp2 = Employee ("Manni", 5000)
+
+# acceder al objeto
+emp1.displayEmployee()
+emp2.displayEmployee()
+
+print ("Total Employee %d" % Employee.empCount)
+
+
+("Employee.__doc__:", Employee.__doc__)
+print ("Employee.__name__:", Employee.__name__)
+print ("Employee.__module__:", Employee.__module__)
+print ("Employee.__bases__:", Employee.__bases__)
+print ("Employee.__dict__:", Employee.__dict__)
