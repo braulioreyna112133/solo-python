49be07fe5328f3ca719905b98a70244b2fbbb741
brauliolucia <5531276172brc@gmail.com>
2025-04-06 23:53:42 -0600

    subiendo poo3

--- poo3.py
+++ poo3.py
@@ -6,3 +6,6 @@ class Pilots:
 
 pilot1 = Pilots (234, 'Full')
 pilot2 = Pilots (777, 'Full')
+
+# Name Mangling	__variable	Se renombra internamente (_Clase__variable)	
+# Cuando quieres evitar colisiones de nombres en herencia.
