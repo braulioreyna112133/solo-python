45f8897466e7869c80ad57f5fe3190f5a6fc1b71
brauliolucia <5531276172brc@gmail.com>
2025-04-08 04:21:55 -0600

    subiendo script poo5 sobre classmethods aun vació

--- poo4.py
+++ poo4.py
@@ -12,15 +12,22 @@ class pilotTest:
     def __init__ (self, request):
         
         # 5 definir variable de instancia
-        self.request = request
-        request = 'aña'
+        self.request = request # es como una variable vacía
 
     def looking (self):
         print(f"{self.request} es un escenario observable")
 
-    # def info (self):
-    #     print(f"{self.request} es observada por un {self.pilot}")
+    def info (self):
+        print(f"{self.request} es observada por un {self.pilot}")
 
-user = pilotTest()
+user = pilotTest("Area X")
 user.looking()  
-# user.info()    
+user.info()    
+
+
+# Se llama a la clase y se pasa "Area X" como argumento posicional al método __init__,
+# el cual inicializa la variable de instancia `self.request` con ese valor.
+
+# La variable de instancia `self.request` no está vacía. Se inicializa con el valor
+# proporcionado al crear la instancia (en este caso, "Area X"). Si no se proporciona
+# un argumento al instanciar la clase, se generará un error porque el argumento es obligatorio.
