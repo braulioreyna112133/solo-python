2b9a26bd7de57f3d186c48a965723cbcf4c919ff
brauliolucia <5531276172brc@gmail.com>
2025-04-12 22:23:41 -0600

    actualizado

--- poo5-1.py
+++ poo5-1.py
@@ -1,2 +1,32 @@
-object = input ()
-print (object)
+
+# Ejemplo 2: usando decorador classmethod, forma 1
+
+# classmethod(instance_method)
+# Usa un método de instancia cuando necesitas 
+# trabajar con datos específicos de un objeto.
+
+# Usa un método de clase cuando necesitas modificar 
+# algo a nivel de clase o crear objetos de forma especial.
+
+class objects:
+    human = 'human'
+
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+
+    def show (self):
+        print (f'Type: {self.human}: {self.name}, {self.age}')
+
+    @classmethod
+    def addPatholgy (cls, pathology):
+        cls.human = pathology
+
+# llamada de clase
+user = objects ('Braulio', 21)
+user.show()
+
+# Braulio presenta un problema
+objects.addPatholgy ('blind human')
+user.show()
+print(objects.human)
--- poo5.py
+++ poo5.py
@@ -1,11 +1,5 @@
 # uso del decorador classmethods()
 
-# classmethod(instance_method)
-# Usa un método de instancia cuando necesitas trabajar con datos específicos de un objeto.
-
-# Usa un método de clase cuando necesitas modificar algo a nivel de clase o crear objetos de forma especial.
-
-
 # Ejemplo 1: utilizando método de instancia
 class pilotIdentification:
     def __init__ (self, pathology, age, soundLevel):
@@ -16,5 +10,6 @@ class pilotIdentification:
     def outputConfig (self):
         print (f'Info user config: {self.pathology}, {self.age}, {self.soundLevel}')
 
-userConfig = pilotIdentification ('full blind user', 5, 'higha adaptable')
+userConfig = pilotIdentification ('full blind user', 5, 'high-adaptable')
 userConfig.outputConfig()
+
