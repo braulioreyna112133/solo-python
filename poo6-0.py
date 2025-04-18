e0dad5365e2afdeff660e7dde8a84f4708385db1
brauliolucia <5531276172brc@gmail.com>
2025-04-17 22:37:47 -0600

    actualizacion solo de subida

--- poo6-0.py
+++ poo6-0.py
@@ -7,7 +7,7 @@ class userVision:
 # Definir un método de clase -> recibe la clase como primer argumento (cls)
 @classmethod
 def userVisionMethod(cls):
-    print('The user\'s vision worsened to total blindness')
+    print('The users vision worsened to total blindness')
 
 # Usar la función setattr(objeto, nombre, valor) para agregar el método a la clase
 # Nota: al agregar un @classmethod manualmente, debemos usar 'classmethod' explícitamente
