df39a356dcf944433029ce712333974a6553363b
brauliolucia <5531276172brc@gmail.com>
2025-06-11 07:03:44 -0600

    .

---
+++ poo10-dalto-min61.py
@@ -0,0 +1,9 @@
+'''Herencia: hereda todo lo de la clase padre a la clase anterior'''
+'''Primer ejercicio de herencia'''
+
+class Human:
+    '''constructor'''
+    def __init__ (self, name, age, disability):
+        self.name = name
+        self.age = age
+        self.disability = disability
